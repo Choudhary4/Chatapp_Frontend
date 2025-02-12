12d689118ac36471f10c0de6c7b08580cd732ef3
Saksham Goyal <sakshamgoyal1510@gmail.com>
2025-02-13 03:48:26 +0530

    updated Base URL Issue

--- src/hooks/useGetOtherUsers.jsx
+++ src/hooks/useGetOtherUsers.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect } from 'react';
-import axios from "axios";
-import { useDispatch } from "react-redux";
+// useGetOtherUsers.jsx
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { setOtherUsers } from '../redux/userSlice';
 import { BASE_URL } from '..';
 
+// Use environment variable for BASE_URL
 
 const useGetOtherUsers = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                const res = await axios.get(`${BASE_URL}/api/v1/user`, {
-                    withCredentials: true // Ensure cookies are sent
-                });
-                console.log("other users -> ", res.data);
-                dispatch(setOtherUsers(res.data));
-            } catch (error) {
-                console.log("Error fetching other users:", error);
-            }
-        }
-        
-        fetchOtherUsers();
-    }, [])
+  const dispatch = useDispatch();
 
-}
+  useEffect(() => {
+    const fetchOtherUsers = async () => {
+      try {
+        const res = await axios.get(`${BASE_URL}/api/v1/user`, {
+          // Ensure credentials (cookies) are sent with the request
+          withCredentials: true,
+        });
+        console.log("Other users -> ", res.data);
+        dispatch(setOtherUsers(res.data));
+      } catch (error) {
+        console.error("Error fetching other users:", error);
+      }
+    };
 
-export default useGetOtherUsers
+    fetchOtherUsers();
+  }, [dispatch]);
+};
+
+export default useGetOtherUsers;
